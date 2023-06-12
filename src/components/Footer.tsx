de9dfdc76d6cbc2d241a5f218cc65a0e9111fd3e
cafe51 <cafecafe51@hotmail.com>
2023-06-12 16:41:51 -0400

    footer estilizado

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,18 @@ const FooterWrap = styled.footer`
   flex-direction: column;
   color: white;
   text-align: center;
-  border: 1px solid white;
-  width: 99%;
-
-  background-color: #54008B;
+  width: 100%;
+  padding-bottom: 20px;
+  background: radial-gradient(
+      89.23% 1958.28% at 95.38% 49.55%,
+      #56008D 2.32%,
+      #350B53 16.93%,
+      #221828 38.86%,
+      #1F1523 63.89%,
+      #24162D 72.69%,
+      #25162E 81.14%
+    );
+  box-shadow: 0px -4px 4px rgba(0, 0, 0, 0.8);
 `;
 
 class Footer extends React.Component {
