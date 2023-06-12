b53bbbaa4dd8915e33731454982fa1c8d5c68202
cafe51 <cafecafe51@hotmail.com>
2023-06-12 17:03:33 -0400

    estilos de Footer.tsx realocados para FooterStyles.ts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { TbHexagonLetterJ } from 'react-icons/tb';
+import { FooterWrap, SocialMediaContainer } from '../styles/FooterStyles';
 
-
-const FooterWrap = styled.footer`
-  display: flex;
-  flex-direction: column;
-  color: white;
-  text-align: center;
-  width: 100%;
-  padding-bottom: 20px;
-  background: radial-gradient(
-      89.23% 1958.28% at 95.38% 49.55%,
-      #56008D 2.32%,
-      #350B53 16.93%,
-      #221828 38.86%,
-      #1F1523 63.89%,
-      #24162D 72.69%,
-      #25162E 81.14%
-    );
-  box-shadow: 0px -4px 4px rgba(0, 0, 0, 0.8);
-`;
-
-const SocialMediaContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 32px;
-`;
 
 class Footer extends React.Component {
   render() {
---
+++ src/styles/FooterStyles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const FooterWrap = styled.footer`
+  display: flex;
+  flex-direction: column;
+  color: white;
+  text-align: center;
+  width: 100%;
+  padding-bottom: 20px;
+  background: radial-gradient(
+      89.23% 1958.28% at 95.38% 49.55%,
+      #56008D 2.32%,
+      #350B53 16.93%,
+      #221828 38.86%,
+      #1F1523 63.89%,
+      #24162D 72.69%,
+      #25162E 81.14%
+    );
+  box-shadow: 0px -4px 4px rgba(0, 0, 0, 0.8);
+`;
+
+export const SocialMediaContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+`;
